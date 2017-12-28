3036efc5dad6f53f051ead8ccd757d4b89d36f5f
Vinidicta <malgorzatakaleta1990@gmail.com>
2017-12-28 22:32:58 +0100

    Part 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,28 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.Calculator.Calculator;
+
 public class TestingMain {
 
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(3, 5);
+
+        if (addResult == 8) {
+            System.out.println("Code correct!");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int substractResult = calculator.substract(5, 1);
+
+        if (substractResult == 4) {
+            System.out.println("Code correct!");
+        } else {
+            System.out.println("Error!");
+        }
+
     }
 }
