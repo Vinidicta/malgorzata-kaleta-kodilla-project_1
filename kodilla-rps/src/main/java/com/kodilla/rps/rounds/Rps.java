cd036b0c9269235d80af892c5e0b2e58b9743633
Vinidicta <malgorzatakaleta1990@gmail.com>
2018-04-11 20:35:55 +0200

    RPS update - bug fixing

--- kodilla-rps/src/main/java/com/kodilla/rps/rounds/Rps.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/rounds/Rps.java
@@ -12,13 +12,21 @@ public class Rps {
     private int pcWins;
     private int rounds;
 
-
     public Rps() {
+        do {
+            input = new Scanner(System.in);
+            rounds = getRounds();
+        } while (rounds < 0);
+    }
 
-                input = new Scanner(System.in);
-                System.out.println("Please put how much rounds would you like to play. To accept press enter");
-                rounds = input.nextInt();
-
+    private int getRounds() {
+        try {
+            System.out.println("Please put how much rounds would you like to play. To accept press enter");
+            return Integer.valueOf(input.nextLine());
+        } catch (RuntimeException e) {
+            System.out.println("Wrong type of data");
+            return -1;
+        }
     }
 
     public void startGame() {
